407c3c8ff58a9a9e5cc37ddf8bcffee53663d0f3
kandula-prakash <abhinayprakash143@gmail.com>
2021-10-26 10:11:00 +0530

    feat:abstraction

---
+++ abstraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace abstraction
+{
+    public abstract class car
+    {
+        //abstrct mathod without any body
+        public abstract void carColor();
+        //regular method
+        public void name()
+        {
+            Console.WriteLine("jupiter");
+        }
+    }
+    //derived class(inherit from animal)
+    class swift : car
+    {
+        public override void carColor()
+        {
+            //the body of the car color() is provide here
+            Console.WriteLine("merron");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            swift s = new swift();
+            s.carColor();
+            s.name();
+            
+        }
+    }
+}
