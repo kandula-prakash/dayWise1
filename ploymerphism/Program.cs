2db6486d011a568b5d1ee964da7467a0af047b24
kandula-prakash <abhinayprakash143@gmail.com>
2021-10-29 23:02:41 +0530

    feat:polymorphism

---
+++ ploymerphism/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ploymerphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("finding the area of square\n");
+
+            area a = new area();
+            a.side = 5;
+            a.findSquare();
+            Console.WriteLine();
+
+            area obj = new area();
+            obj.side = 5;
+            obj.findSquare();
+            Console.ReadLine();
+        }
+    }
+    class area
+    {
+        public int side { get; set; }
+
+        public virtual void findSquare()
+        {
+            
+            int areaOfTheSquare = side * side;
+            Console.WriteLine("areaOfTheSquare"+ areaOfTheSquare);
+        }
+    }
+
+     class side:area 
+    {
+        public override void findSquare()
+        {
+            int areaOfTheSquare = side * side;
+            Console.WriteLine("areaOfTheSquare"+areaOfTheSquare);
+        }
+    }
+}
---
+++ ploymerphism/Shape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ploymerphism
+{
+    class milk
+    {
+        public virtual void Draw()
+        {
+
+        }
+    }
+    class curd : milk
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("curd Deawn");
+        }
+    }
+    class butterMilk : milk
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("butterMilk Drawn");
+        }
+    }
+    class ghee : milk
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("ghee draw");
+        }
+    }
+
+    class shape
+    {
+       // static void Main(string[] args)
+        {
+            milk[] m = new milk[2];
+            m[0] = new ghee();
+            m[1] = new butterMilk();
+            Console.WriteLine("\n\n runtime polymorphism test\n\n");
+            for (int i = 0; i < 2; i++)
+            {
+                m[i].Draw();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
