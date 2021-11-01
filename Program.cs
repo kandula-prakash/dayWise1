eefefea11f0a1ebec1fd79b805494b8a9d3c4ceb
kandula-prakash <abhinayprakash143@gmail.com>
2021-11-01 10:01:20 +0530

    feat:lamdaExpression

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lamdaExpression
+{
+    class Program
+    {
+        public delegate string WishDelegate(string name);
+      
+        static void Main(string[] args)
+        {
+          
+            WishDelegate wd = (name) =>
+            {
+                return "hello Mr." + name + "have a nice day";
+            };
+            string WishDelegate = wd.Invoke("manikanta");
+            Console.WriteLine(WishDelegate);
+            Console.ReadKey();
+
+          //  WishDelegate wd = Program.sayhello;
+        }
+    }
+}
