677aa74c5d02a22ea6d68ce0749a9ae24346470b
kandula-prakash <abhinayprakash143@gmail.com>
2021-11-27 10:12:18 +0530

    feat:multilevel

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace multileviInheritance
+{
+    class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                Son s = new Son();
+                s.Display();
+                s.DisplayOne();
+                s.DisplayTwo();
+                Console.Read();
+            }
+        }
+        class Grandfather
+        {
+            public void Display()
+            {
+                Console.WriteLine("Grandfather...");
+            }
+        }
+        class Father : Grandfather
+        {
+            public void DisplayOne()
+            {
+                Console.WriteLine("Father...");
+            }
+        }
+    class Son : Father
+    {
+        public void DisplayTwo()
+        {
+            Console.WriteLine("Son.. ");
+        }
+
+    }
+}
