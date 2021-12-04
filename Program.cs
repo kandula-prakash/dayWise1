7c6bcc4ffff10d3755be13202ff262de495cd978
kandula-prakash <abhinayprakash143@gmail.com>
2021-12-04 09:35:10 +0530

    feat:dataStructure

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace linkedListDemoBRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            singleLinedList s = new singleLinedList();
+            s.AddEmptyList(20);
+            s.AddLast(40);
+            s.begining(30);
+            int x;
+            Console.WriteLine("enter the postion");
+            x = int.Parse(Console.ReadLine());
+            s.AtPosition(10, 2);
+            s.AddBefore(40, 3);
+            s.deleteFirst();
+            
+            s.Display();
+
+        }
+    }
+}
