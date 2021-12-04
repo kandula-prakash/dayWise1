f9651b47e839c2b86f8759903c89b40f404d6f46
kandula-prakash <abhinayprakash143@gmail.com>
2021-12-04 09:31:57 +0530

    feat:daywise

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace daywiseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x<=100)
+            {
+                int y = 2;
+                while (y<=x)
+                {
+                    if (x==y)
+                    {
+                        Console.WriteLine(x);
+                    }
+                    if (x%y==0)
+                    {
+                        break;
+                    }
+                    y++;
+                }
+                x++;
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
+        
+    
+
