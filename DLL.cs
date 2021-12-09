9d402ac0253c3c0cb0d383a697493d2b32e96ed1
kandula-prakash <abhinayprakash143@gmail.com>
2021-12-09 10:26:33 +0530

    doubleLinkedList

---
+++ DLL.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace doubleLinkedList
+{
+    public class DLL
+    {
+        public Node head;
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node prev;
+            int temp_data;
+            public Node(int d)
+            {
+                data = d;
+            }
+            public Node()
+            {
+                data = temp_data;
+            }
+        }
+        public void display()
+        {
+            Node new_node = head;
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+            }
+            while (new_node != null)
+            {
+                Console.WriteLine(new_node.data + " ");
+                new_node = new_node.next;
+            }
+        }
+        public void push(int new_data)
+        {
+            Node new_Node = new Node(new_data);
+            new_Node.next = head;
+            new_Node.prev = null;
+            if (head != null)
+            {
+                head.prev = new_Node;
+
+            }
+            head = new_Node;
+            Console.WriteLine("first node added sucessfully");
+        }
+
+        public void AddLast(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            Node last = head;
+            new_node.next = null;
+            if (head == null)
+            {
+                new_node.prev = null;
+                head = new_node;
+                return;
+            }
+            while (last.next != null)
+            {
+                last = last.next;
+            }
+
+            //change the next of last Node
+            last.next = new_node;
+            //make the last node as previous of new node
+            new_node.prev = last;
+            Console.WriteLine("last number");
+
+
+
+        }
+        public void deleteAtFirst()
+        {
+            if (head!=null)
+            {
+                if (head.next==null)
+                {
+                    head = null;
+
+                }
+                else
+                {
+                    Node temp = new Node();
+                    temp = head;
+                    while (temp.next.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    Node lastNode = temp.next;
+                    lastNode = null;
+                    temp.next = null;
+
+                }
+            }
+          
+        }
+        public Node findElement(int searchKey)
+        {
+            while (this.head!=null)
+            {
+                if (this.head.data == searchKey)
+                {
+                    return this.head;
+                }
+                    this.head = this.head.next;
+                    Console.WriteLine("element{0} found in list...", this.head.data);
+                
+              
+            }
+
+            return null;
+           
+
+
+        }
+        public Node insertSortedList(int value)
+        {
+            Node newNode = new Node(value);
+            if (head==null)
+            {
+                return newNode;
+            }
+            Node current = head;
+            Node temp = null;
+            while (current!=null&&current.data<newNode.data)
+            {
+                temp = current;
+                current = current.next;
+            }
+            newNode.next = current;
+            temp.next = newNode;
+            return head;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace doubleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DLL d = new DLL();
+           
+            d.push(10);
+            d.push(20);
+            d.push(30);
+            d.push(40);
+            d.AddLast(60);
+            d.deleteAtFirst();
+            
+            d.findElement(20);
+           
+            d.display();
+            d.insertSortedList(56);
+            Console.WriteLine( "******************");
+            d.display();
+
+        }
+    }
+}
